01cef725ecbc0ce45d40e057ac5d991c9299d67c
Sara <58045959+sarabajraktari@users.noreply.github.com>
2024-11-13 20:53:52 +0100

    added contactCoach

---
+++ src/store/modules/requests/index.js
@@ -0,0 +1,29 @@
+export default {
+    namespaced:true,
+
+    state(){
+        return{
+            requests:[]
+        }
+    },
+    mutations:{
+        addRequest(state, payload){
+            state.requests.push(payload);
+        }
+    },
+    actions:{
+        contactCoach(context, payload){
+            const newRequest = {
+                id: new Date().toISOString(),
+                coachId: payload.coachId,
+                userEmail: payload.email,
+                message: payload.message
+            };
+
+            context.commit('addRequest', newRequest);
+        }
+    },
+    getters:{
+        
+    }
+}
