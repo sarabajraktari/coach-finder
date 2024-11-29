d42f6b4899f37f49efd693c66100d6ecb260fbd3
Sara <58045959+sarabajraktari@users.noreply.github.com>
2024-11-29 08:43:10 +0100

    auth part

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,15 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js';
+
 const store = createStore({
 
     modules: { 
         coaches: coachesModule,
-        requests: requestsModule
+        requests: requestsModule,
+        auth: authModule,
     },
-    state(){
-        return {
-            coachId: 'c3',
-        }
-    },
-    getters:{
-        coachId(state){
-            return state.coachId;
-        }
-    }
 });
 
 
